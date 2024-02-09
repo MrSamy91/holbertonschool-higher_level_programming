9813c8f3d74c53e2db412ffd78f5223963303833
MrSamy91 <samy.ajouid@gmail.com>
2024-02-09 15:06:12 +0100

    repushing new version

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,14 +2,13 @@
 from sys import argv
 
 if __name__ == "__main__":
-    argc = len(argv) - 1
-
-    if argc == 0:
-        print("0 arguments.")
-    elif argc == 1:
+    args = len(argv) - 1
+    if args > 1:
+        print("{} arguments:".format(args))
+        for i in range(1, args + 1):
+            print("{}: {}".format(i, argv[i]))
+    elif args == 1:
         print("1 argument:")
+        print("1: {}".format(argv[1]))
     else:
-        print("{:d} arguments:".format(argc))
-
-    for i in range(1, argc + 1):
-        print("{:d}: {}".format(i, argv[i]))
+        print("0 arguments.")
