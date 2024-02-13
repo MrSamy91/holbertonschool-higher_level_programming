99830d057654e94c9efb2e10b3eaaa721a2ea1e4
MrSamy91 <samy.ajouid@gmail.com>
2024-02-13 16:25:26 +0100

    adding all the last week files

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-
-def print_list_integer(my_list=[]):
-    """Prints all integers of a list."""
-    for number in my_list:
-        print("{:d}".format(number))
-
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    print_list_integer(my_list)
+def print_list_integer(my_list=None):
+    for num in my_list:
+        print("{:d}".format(num))
