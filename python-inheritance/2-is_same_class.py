682489b069c98b451fd1e9489043cb4b9ae77bca
MrSamy91 <samy.ajouid@gmail.com>
2024-02-28 13:52:44 +0100

    addig all inheritence tasks

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Define the function is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if the object is exactly an 
+    instance of the specified class
+    """
+    return type(obj) is a_class
