0fe3cbf050946d710b21545be8d7fb130d80bad5
MrSamy91 <samy.ajouid@gmail.com>
2024-05-15 15:19:21 +0200

    fixing pycodestyle

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Pad the tuples to ensure they have at least two elements
     padded_tuple_a = tuple_a + (0, 0)
     padded_tuple_b = tuple_b + (0, 0)
-    
     # Add corresponding elements from each tuple
-    sum_tuple = (padded_tuple_a[0] + padded_tuple_b[0], padded_tuple_a[1] + padded_tuple_b[1])
-    
+    sum_tuple = (
+        padded_tuple_a[0] + padded_tuple_b[0],
+        padded_tuple_a[1] + padded_tuple_b[1]
+    )
     return sum_tuple
