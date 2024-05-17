66645945659d747bf8f4138c6a1d71e2b0414021
MrSamy91 <samy.ajouid@gmail.com>
2024-05-17 16:10:37 +0200

    fix checker again

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    # Create a new matrix with the same dimensions as the input matrix
-    new_matrix = []
-    
-    for row in matrix:
-        # Square each element in the row and append the new row to the new matrix
-        new_matrix.append([x ** 2 for x in row])
-    
-    return new_matrix
+    # Create a new matrix by squaring each element of the input matrix
+    return [[element ** 2 for element in row] for row in matrix]
