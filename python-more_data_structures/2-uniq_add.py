ada06caf3ed2265bbc7a3a4b6d758a1ee019cc36
MrSamy91 <samy.ajouid@gmail.com>
2024-05-17 16:41:47 +0200

    maj file 2

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#maj
+
+def uniq_add(my_list=[]):
+    # Create a set to store unique integers
+    unique_integers = set()
+    
+    # Add each element of the list to the set (duplicates will be ignored)
+    for element in my_list:
+        unique_integers.add(element)
+    
+    # Sum all unique integers and return the result
+    return sum(unique_integers)
