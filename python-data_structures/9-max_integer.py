35d1fb7f293c27bbb6da7c63728d0b04394df719
MrSamy91 <samy.ajouid@gmail.com>
2024-07-12 16:30:45 +0200

    adding lasts tasks

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,10 +4,9 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
 
-    max_num = my_list[0]
-    for num in my_list[1:]:
-        if num > max_num:
-            max_num = num
+    max_value = my_list[0]
+    for number in my_list:
+        if number > max_value:
+            max_value = number
 
-
-return max_num
+    return max_value
